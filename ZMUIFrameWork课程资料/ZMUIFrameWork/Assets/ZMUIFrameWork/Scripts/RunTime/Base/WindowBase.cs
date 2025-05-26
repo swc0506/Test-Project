9bafc9e94098bffb51f6a71926c5f9454bd5c8ad
swc <10920889364@qq.com>
2025-05-26 20:35:51 +0800

    课程资料

--- ZMUIFrameWork课程资料/ZMUIFrameWork/Assets/ZMUIFrameWork/Scripts/RunTime/Base/WindowBase.cs
+++ ZMUIFrameWork课程资料/ZMUIFrameWork/Assets/ZMUIFrameWork/Scripts/RunTime/Base/WindowBase.cs
@@ -1,35 +1,52 @@
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-namespace ZMUIFrameWork.Scripts.RunTime.Base;
-
 public class WindowBase : WindowBehaviour
 {
-    private List<Button> mAllButtonList = new List<Button>();
-    private List<Toggle> mToggleList = new List<Toggle>();
-    private List<InputField> mInputList = new List<InputField>();
-    
-    #region 生命周期
+    private List<Button> mAllButtonList = new List<Button>();//所有Button列表
+    private List<Toggle> mToggleList = new List<Toggle>();//所有的Toggle列表
+    private List<InputField> mInputList = new List<InputField>();//所有的输入框列表
 
+    private CanvasGroup mUIMask;
+    private CanvasGroup mCanvasGroup;
+    protected Transform mUIContent;
+    protected bool mDisableAnim = false;//禁用动画
+    /// <summary>
+    /// 初始化基类组件
+    /// </summary>
+    private void InitializeBaseComponent()
+    {
+        mCanvasGroup = transform.GetComponent<CanvasGroup>();
+        mUIMask = transform.Find("UIMask").GetComponent<CanvasGroup>();
+        mUIContent = transform.Find("UIContent").transform;
+    }
+    #region 声明周期
     public override void OnAwake()
     {
         base.OnAwake();
+        InitializeBaseComponent();
+         
     }
-
     public override void OnShow()
     {
         base.OnShow();
+        ShowAnimation();
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
+
     }
 
     public override void OnHide()
     {
         base.OnHide();
+
     }
 
     public override void OnDestroy()
@@ -42,16 +59,57 @@ public class WindowBase : WindowBehaviour
         mToggleList.Clear();
         mInputList.Clear();
     }
-
     #endregion
-
-    public override void SetVisible(bool isVisible)
+    #region 动画管理
+    public void ShowAnimation()
     {
-        GameObject.SetActive(isVisible);//后续修改
+        //基础弹窗不需要动画
+        if (Canvas.sortingOrder > 90 && mDisableAnim == false)
+        {
+            //Mask动画
+            mUIMask.alpha = 0;
+            mUIMask.DOFade(1, 0.2f);
+            //缩放动画
+            mUIContent.localScale = Vector3.one * 0.8f;
+            mUIContent.DOScale(Vector3.one, 0.3f).SetEase(Ease.OutBack);
+        }
     }
-
-    #region 事件监听管理
-
+    public void HideAnimation()
+    {
+        if (Canvas.sortingOrder > 90 && mDisableAnim == false)
+        {
+            mUIContent.DOScale(Vector3.one * 1.1f, 0.2f).SetEase(Ease.OutBack).OnComplete(() =>
+            {
+                UIModule.Instance.HideWindow(Name);
+            });
+        }
+        else
+        {
+            UIModule.Instance.HideWindow(Name);
+        }
+    }
+    #endregion
+    public void HideWindow()
+    {
+        HideAnimation();
+        //UIModule.Instance.HideWindow(Name);
+    }
+    public override void SetVisible(bool isVisble)
+    {
+        //gameObject.SetActive(isVisble);//临时代码
+        mCanvasGroup.alpha = isVisble ? 1 : 0;
+        mCanvasGroup.blocksRaycasts = isVisble;
+        Visible = isVisble;
+    }
+    public void SetMaskVisible(bool isVisble)
+    {
+        if (!UISetting.Instance.SINGMASK_SYSTEM)
+        {
+            return;
+        }
+        mUIMask.alpha = isVisble ? 1 : 0;
+    }
+    #region 事件管理
     public void AddButtonClickListener(Button btn, UnityAction action)
     {
         if (btn != null)
@@ -62,9 +120,9 @@ public class WindowBase : WindowBehaviour
             }
             btn.onClick.RemoveAllListeners();
             btn.onClick.AddListener(action);
+
         }
     }
-
     public void AddToggleClickListener(Toggle toggle, UnityAction<bool, Toggle> action)
     {
         if (toggle != null)
@@ -74,13 +132,13 @@ public class WindowBase : WindowBehaviour
                 mToggleList.Add(toggle);
             }
             toggle.onValueChanged.RemoveAllListeners();
-            toggle.onValueChanged.AddListener(isOn =>
+            toggle.onValueChanged.AddListener((isOn) =>
             {
-                action?.Invoke(isOn ,toggle);
+                action?.Invoke(isOn, toggle);
             });
+
         }
     }
-    
     public void AddInputFieldListener(InputField input, UnityAction<string> onChangeAction, UnityAction<string> endAction)
     {
         if (input != null)
@@ -95,31 +153,30 @@ public class WindowBase : WindowBehaviour
             input.onEndEdit.AddListener(endAction);
         }
     }
-
     public void RemoveAllButtonListener()
     {
-        foreach (var btn in mAllButtonList)
+        foreach (var item in mAllButtonList)
         {
-            btn.onClick.RemoveAllListeners();
+            item.onClick.RemoveAllListeners();
         }
     }
 
     public void RemoveAllToggleListener()
     {
-        foreach (var toggle in mToggleList)
+        foreach (var item in mToggleList)
         {
-            toggle.onValueChanged.RemoveAllListeners();
+            item.onValueChanged.RemoveAllListeners();
         }
     }
 
     public void RemoveAllInputListener()
     {
-        foreach (var input in mInputList)
+        foreach (var item in mInputList)
         {
-            input.onValueChanged.RemoveAllListeners();
-            input.onEndEdit.RemoveAllListeners();
+            item.onValueChanged.RemoveAllListeners();
+            item.onEndEdit.RemoveAllListeners();
         }
+
     }
-    
     #endregion
 }
