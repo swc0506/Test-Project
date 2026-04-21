d54efcda8fd0e31c7b3eefd20a74c0f61fd186ed
swc <10920889364@qq.com>
2026-04-21 20:54:52 +0800

    HOT

--- AssetHot/Assets/AssetsFrame/RunTime/BundleLoad/ResourceManager.cs
+++ AssetHot/Assets/AssetsFrame/RunTime/BundleLoad/ResourceManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace ZM.AssetFrameWork
@@ -8,6 +9,7 @@ namespace ZM.AssetFrameWork
         // 已经加载的资源
         private Dictionary<uint, BundleItem> mAlreadyLoadAssetsDic = new Dictionary<uint, BundleItem>();
 
+        #region 加载资源
         /// <summary>
         /// 同步加载资源，外部直接调用，仅仅加载不需要实例化的资源
         /// </summary>
@@ -67,6 +69,80 @@ namespace ZM.AssetFrameWork
         }
 
         /// <summary>
+        /// 异步加载资源，外部直接调用，仅仅加载不需要实例化的资源
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="callback"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public void LoadResourceAsync<T>(string path, Action<UnityEngine.Object> callback) where T : UnityEngine.Object
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                Debug.LogError("资源路径为空");
+                callback?.Invoke(null);
+            }
+
+            uint crc = Crc32.GetCrc32(path);
+            BundleItem item = GetCacheBundleItemFromDic(crc);
+
+            if (item.obj != null)
+            {
+                callback?.Invoke(item.obj as T);
+            }
+
+            //声明新对象
+            T obj = null;
+
+#if UNITY_EDITOR
+            if (BundleSettings.Instance.loadAssetType == LoadAssetEnum.Editor)
+            {
+                obj = LoadAssetsFormEditor<T>(path);
+                callback?.Invoke(obj);
+            }
+#endif
+            if (obj == null)
+            {
+                item = AssetBundleManager.Instance.LoadAssetBundle(crc);
+                if (item != null)
+                {
+                    if (item.obj != null)
+                    {
+                        callback?.Invoke(item.obj);
+                        item.bundlePath = path;
+                        item.crc = crc;
+                        mAlreadyLoadAssetsDic[crc] = item;
+                    }
+                    else
+                    {
+                        // 异步加载
+                        AssetBundleRequest request = item.assetBundle.LoadAssetAsync<T>(item.assetName);
+                        request.completed += (asyncOperation) =>
+                        {
+                            UnityEngine.Object loadObj = ((AssetBundleRequest)asyncOperation).asset;
+                            item.obj = loadObj;
+                            item.bundlePath = path;
+                            item.crc = crc;
+                            mAlreadyLoadAssetsDic[crc] = item;
+                            callback?.Invoke(loadObj);
+                        };
+                    }
+                }
+                else
+                {
+                    Debug.LogError("资源加载失败");
+                    callback?.Invoke(null);
+                }
+            }
+            else
+            {
+                item.obj = obj;
+                item.bundlePath = path;
+                mAlreadyLoadAssetsDic[crc] = item;
+            }
+        }
+        
+        /// <summary>
         /// 通过资源路径获取缓存的BundleItem
         /// </summary>
         /// <param name="crc"></param>
@@ -88,5 +164,7 @@ namespace ZM.AssetFrameWork
             return UnityEditor.AssetDatabase.LoadAssetAtPath<T>(path);
         }
 #endif
+        
+        #endregion
     }
 }
